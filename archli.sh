7b57a4f171871e269b4d974526cd61b0eff24fa9
Andrew Redinbo <atredinbo@gmail.com>
2019-10-28 14:46:46 -0400

    added man because base is now useless i guess

--- archli.sh
+++ archli.sh
@@ -27,7 +27,7 @@ mkdir -p /mnt/boot/esp
 mount /dev/sda1 /mnt/boot/esp
 
 # Pacstrap main installation
-pacstrap /mnt base linux linux-firmware base-devel grub efibootmgr git vim ansible networkmanager
+pacstrap /mnt base linux linux-firmware base-devel grub efibootmgr man git vim ansible networkmanager
 
 # Generate filesystem table
 genfstab -U /mnt >> /mnt/etc/fstab
